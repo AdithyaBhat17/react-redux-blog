3e3343f319751cd9fbddd44bee3d67fdebb60c2b
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-02-23 15:51:39 +0530

    :sparkles: return articles for users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import { articles } from "./articles";
 
 export default combineReducers({
-    //Store
+  //Store
+  articles
 });
