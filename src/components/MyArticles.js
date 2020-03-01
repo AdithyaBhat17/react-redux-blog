44ab64f0993ca99c1403b15652c891392d2b3d6e
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-01 18:52:57 +0530

    :sparkles: completed recent posts

--- src/components/MyArticles.js
+++ src/components/MyArticles.js
@@ -2,16 +2,18 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Grid } from "semantic-ui-react";
 
-import { getUserArticles } from "../actions";
+import { getUserArticles, getPublishedArticles } from "../actions";
 import ArticlesList from "./ArticlesList";
+import RecentPosts from "./RecentPosts";
 
 class MyArticles extends Component {
   componentDidMount() {
     this.props.getUserArticles();
+    this.props.getPublishedArticles();
   }
 
   render() {
-    const { articles } = this.props;
+    const { articles, recent } = this.props;
 
     return (
       <Fragment>
@@ -21,7 +23,9 @@ class MyArticles extends Component {
             <Grid.Column width={12}>
               <ArticlesList articles={articles} />
             </Grid.Column>
-            <Grid.Column width={4}>{/* @todo Recent Posts Card */}</Grid.Column>
+            <Grid.Column width={4}>
+              <RecentPosts recent={recent} />
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </Fragment>
@@ -31,8 +35,12 @@ class MyArticles extends Component {
 
 function mapStateToProps({ articles }) {
   return {
-    articles: articles.user
+    articles: articles.user,
+    recent: articles.published
   };
 }
 
-export default connect(mapStateToProps, { getUserArticles })(MyArticles);
+export default connect(mapStateToProps, {
+  getUserArticles,
+  getPublishedArticles
+})(MyArticles);
