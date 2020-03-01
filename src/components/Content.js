9f54af4e573b61a935d51d6eff640b943cf1caab
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-01 18:51:01 +0530

    :sparkles: completed protected routes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,58 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 import { Switch, Route } from "react-router-dom";
 
-import Feed from './Feed';
-import MyArticles from './MyArticles';
-import NewArticle from './NewArticle';
-import Login from './Login';
+import Feed from "./Feed";
+import MyArticles from "./MyArticles";
+import NewArticle from "./NewArticle";
+import Login from "./Login";
+import ProtectedRoute from "./ProtectedRoute";
+import client from "../utils/firebase";
+import { connect } from "react-redux";
 
-export default class Content extends Component {
-    render() {
-        return (
-            <Container>
-                <Switch>
-                    <Route path="/" exact component={Feed} />
-                    <Route path="/articles" exact component={MyArticles} />
-                    <Route path="/articles/new" exact component={NewArticle} />
-                    <Route path="/login" exact component={Login} />
-                </Switch>
-            </Container>
-        )
-    }
+import { userAuthenticated } from "../actions";
+
+class Content extends Component {
+  state = {
+    isAuthenticated: false,
+    user: null
+  };
+  componentDidMount() {
+    this.observer = client.auth().onAuthStateChanged(user => {
+      user && this.props.userAuthenticated(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.observer();
+  }
+
+  render() {
+    let { isAuthenticated } = this.props;
+    return (
+      <Container>
+        <Switch>
+          <Route path="/" exact component={Feed} />
+          <ProtectedRoute
+            path="/articles"
+            isAuthenticated={isAuthenticated}
+            exact
+            component={MyArticles}
+          />
+          <Route path="/articles/new" exact component={NewArticle} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
+      </Container>
+    );
+  }
 }
+
+const mapStateToProps = ({ auth }) => {
+  let { isAuthenticated, user } = auth;
+  return {
+    isAuthenticated,
+    user
+  };
+};
+
+export default connect(mapStateToProps, { userAuthenticated })(Content);
