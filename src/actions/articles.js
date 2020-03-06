8f9b126772927de7539c8ee7adbba4b3907d0d4c
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-06 21:35:49 +0530

    :sparkles: added action creators for errors and loading state

--- src/actions/articles.js
+++ src/actions/articles.js
@@ -6,12 +6,55 @@ import {
   REMOVE_FILTER_TAG,
   CLEAR_FILTERS,
   PUBLISHED_ARTICLE,
-  CLEAR_CURRENT_ARTICLE
-} from './constants';
+  CLEAR_CURRENT_ARTICLE,
+  CREATE_NEW_ARTICLE,
+  SET_ERROR,
+  SET_LOADING
+} from "./constants";
+
+/**
+ * Action creator to set an error message
+ * @param {string} error
+ */
+export const setError = error => {
+  return {
+    type: SET_ERROR,
+    error
+  };
+};
+
+/**
+ * Action creator to show a spinner
+ * @param {boolean} loading
+ */
+export const setLoading = loading => {
+  return {
+    type: SET_LOADING,
+    loading
+  };
+};
+
+/**
+ * An action creator that returns a function to create a new article.
+ * @param {{title: string, tags: string[], body_markdown: string, published: boolean}} article
+ */
+export const createNewArticle = article => {
+  return dispatch => {
+    fetcher(
+      "https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/",
+      "POST",
+      article
+    )
+      .then(response => dispatch({ type: CREATE_NEW_ARTICLE, response }))
+      .catch(error => console.error(error));
+  };
+};
 
 export const getUserArticles = () => {
   return dispatch => {
-    fetcher("https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/me/all")
+    fetcher(
+      "https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/me/all"
+    )
       .then(response =>
         dispatch({
           type: USER_ARTICLES,
@@ -23,7 +66,7 @@ export const getUserArticles = () => {
 };
 
 export const getPublishedArticles = () => dispatch => {
-  fetcher('https://cors-anywhere.herokuapp.com/https://dev.to/api/articles')
+  fetcher("https://cors-anywhere.herokuapp.com/https://dev.to/api/articles")
     .then(response =>
       dispatch({
         type: PUBLISHED_ARTICLES,
@@ -31,23 +74,25 @@ export const getPublishedArticles = () => dispatch => {
       })
     )
     .catch(error => console.error(error));
-}
+};
 
 export const toggleTagsSelection = (tag, isSelected = true) => {
   return {
     type: isSelected ? ADD_FILTER_TAG : REMOVE_FILTER_TAG,
     payload: tag
   };
-}
+};
 
 export const clearFilters = () => {
   return {
     type: CLEAR_FILTERS
-  }
-}
+  };
+};
 
-export const getArticle = (articleId) => async dispatch => {
-  fetcher(`https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/${articleId}`)
+export const getArticle = articleId => async dispatch => {
+  fetcher(
+    `https://cors-anywhere.herokuapp.com/https://dev.to/api/articles/${articleId}`
+  )
     .then(response =>
       dispatch({
         type: PUBLISHED_ARTICLE,
@@ -55,10 +100,10 @@ export const getArticle = (articleId) => async dispatch => {
       })
     )
     .catch(error => console.error(error));
-}
+};
 
 export const clearCurrentArticle = () => {
   return {
     type: CLEAR_CURRENT_ARTICLE
-  }
-}
+  };
+};
