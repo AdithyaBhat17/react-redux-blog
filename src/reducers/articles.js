5ace8f54918f1993ed98a58e4b4b12de27595c5f
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-06 21:37:56 +0530

    :sparkles: completed adding new articles

--- src/reducers/articles.js
+++ src/reducers/articles.js
@@ -5,41 +5,61 @@ import {
   REMOVE_FILTER_TAG,
   CLEAR_FILTERS,
   PUBLISHED_ARTICLE,
-  CLEAR_CURRENT_ARTICLE
-} from '../actions/constants';
+  CLEAR_CURRENT_ARTICLE,
+  CREATE_NEW_ARTICLE,
+  SET_ERROR,
+  SET_LOADING
+} from "../actions/constants";
 
 const articlesState = {
   filterTags: []
-}
+};
 
 export const articles = (state = articlesState, action) => {
   switch (action.type) {
+    case CREATE_NEW_ARTICLE:
+      return {
+        ...state,
+        loading: !action.response,
+        error: action.response.error,
+        created: action.response.error ? false : true
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.error
+      };
     case USER_ARTICLES:
       return {
         ...state,
-        user: action.payload || []
+        userArticles: action.payload || []
       };
     case PUBLISHED_ARTICLES:
       return {
         ...state,
         published: action.payload || []
-      }
+      };
     case ADD_FILTER_TAG:
       return {
         ...state,
         filterTags: [...state.filterTags, action.payload]
-      }
+      };
     case REMOVE_FILTER_TAG:
       return {
         ...state,
         filterTags: state.filterTags?.filter(tag => tag !== action.payload)
-      }
+      };
     case CLEAR_FILTERS:
-      return { ...state, filterTags: [] }
+      return { ...state, filterTags: [] };
     case PUBLISHED_ARTICLE:
-      return { ...state, current: action.payload }
+      return { ...state, current: action.payload };
     case CLEAR_CURRENT_ARTICLE:
-      return { ...state, current: undefined }
+      return { ...state, current: undefined };
     default:
       return state;
   }
