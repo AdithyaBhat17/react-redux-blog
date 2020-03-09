f86924cc70686cc94fa1f5a199d384eb2b52168a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-09 20:45:56 +0530

    removed custom history object

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react'
-import { Menu, Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Menu, Header } from "semantic-ui-react";
 
-import history from '../history';
-import CONFIG from '../config';
+import CONFIG from "../config";
+import { withRouter } from "react-router-dom";
 
-export default class NavBar extends Component {
+class NavBar extends Component {
+  state = {
+    activeItem: undefined
+  };
 
-    state = {
-        activeItem: undefined
-    }
+  handleMenuClick = (e, { path, name }) => {
+    this.props.history.push(path);
+    this.setState({ activeItem: name });
+  };
 
-    handleMenuClick = (e, { path, name }) => {
-        history.push(path);
-        this.setState({ activeItem: name })
-    }
+  renderMenuItems = () =>
+    CONFIG.menu.map((item, index) => (
+      <Menu.Item
+        key={index}
+        as="a"
+        path={item.path}
+        name={item.name}
+        active={
+          window.location.pathname === item.path ||
+          this.state.activeItem === item.name
+        }
+        onClick={this.handleMenuClick}
+      >
+        {item.title}
+      </Menu.Item>
+    ));
 
-    renderMenuItems = () => CONFIG.menu.map((item, index) =>
-        <Menu.Item
-            key={index}
-            as='a'
-            path={item.path}
-            name={item.name}
-            active={window.location.pathname === item.path || this.state.activeItem === item.name}
-            onClick={this.handleMenuClick}
-        >
-            {item.title}
+  render() {
+    return (
+      <Menu borderless>
+        <Menu.Item>
+          <Header as="h1">BLOG</Header>
         </Menu.Item>
-    )
-
-    render() {
-        return (
-            <Menu borderless >
-                <Menu.Item>
-                    <Header as='h1'>BLOG</Header>
-                </Menu.Item>
-                <Menu.Menu position='right'>
-                    {this.renderMenuItems()}
-                </Menu.Menu>
-            </Menu>
-        )
-    }
+        <Menu.Menu position="right">{this.renderMenuItems()}</Menu.Menu>
+      </Menu>
+    );
+  }
 }
+
+export default withRouter(NavBar);
