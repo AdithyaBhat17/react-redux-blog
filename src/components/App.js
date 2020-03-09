2e027f698273014744de49d8d3b4530f91788d1a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-03-09 20:56:28 +0530

    removed custom history object

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react';
-import { Router } from 'react-router-dom';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import NavBar from './NavBar';
-import Content from './Content';
-import history from '../history';
+import NavBar from "./NavBar";
+import Content from "./Content";
 
 export default class App extends Component {
-    render() {
-        return (
-            <Container fluid>
-                <Router history={history}>
-                    <NavBar />
-                    <Content />
-                </Router>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container fluid>
+        <Router>
+          <NavBar />
+          <Content />
+        </Router>
+      </Container>
+    );
+  }
 }
